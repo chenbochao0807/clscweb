d2005e3e391d00c158464ce1faa92938f99dd5af
chenbochao0807 <chenbochao0807@gmail.com>
2026-07-20 21:19:17 +0800

    fix bug,add new widget add created 404 pages custom.Maybe bring morebug,now sharesBottom and Search died

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -2,7 +2,7 @@ import { getCollection } from "astro:content";
 import { siteConfig } from "@/config";
 import { getCategoryList, getSortedPosts, getTagList } from "@utils/content-utils";
 
-function escapeXml(value) {
+function escapeXml(value: string) {
 	return value
 		.replaceAll("&", "&amp;")
 		.replaceAll("<", "&lt;")
@@ -11,19 +11,40 @@ function escapeXml(value) {
 		.replaceAll("'", "&apos;");
 }
 
-function absolute(pathname) {
+function absolute(pathname: string) {
 	return new URL(pathname, siteConfig.siteUrl).href;
 }
 
-function urlEntry(loc, lastmod, images) {
+function urlEntry(
+	loc: string,
+	lastmod?: string,
+	images: {
+		loc: string;
+		title?: string;
+		caption?: string;
+	}[] = [],
+) {
 	let xml = `  <url><loc>${escapeXml(loc)}</loc>`;
-	if (lastmod) xml += `<lastmod>${escapeXml(lastmod)}</lastmod>`;
-	for (const image of images || []) {
-		xml += `<image:image><image:loc>${escapeXml(image.loc)}</image:loc>`;
-		if (image.title) xml += `<image:title>${escapeXml(image.title)}</image:title>`;
-		if (image.caption) xml += `<image:caption>${escapeXml(image.caption)}</image:caption>`;
+
+	if (lastmod) {
+		xml += `<lastmod>${escapeXml(lastmod)}</lastmod>`;
+	}
+
+	for (const image of images) {
+		xml += `<image:image>`;
+		xml += `<image:loc>${escapeXml(image.loc)}</image:loc>`;
+
+		if (image.title) {
+			xml += `<image:title>${escapeXml(image.title)}</image:title>`;
+		}
+
+		if (image.caption) {
+			xml += `<image:caption>${escapeXml(image.caption)}</image:caption>`;
+		}
+
 		xml += `</image:image>`;
 	}
+
 	xml += `</url>`;
 	return xml;
 }
@@ -35,8 +56,13 @@ export async function GET() {
 	const achievements = await getCollection("achievements");
 	const now = new Date().toISOString();
 
-	const entries = [];
-	const achievementImages = [];
+	const entries: string[] = [];
+	const achievementImages: {
+		loc: string;
+		title?: string;
+		caption?: string;
+	}[] = [];
+
 	const staticPages = [
 		"/",
 		"/about/",
@@ -48,47 +74,82 @@ export async function GET() {
 		"/resources/",
 	];
 
+	// Static pages
 	for (const page of staticPages) {
 		entries.push(urlEntry(absolute(page), now));
 	}
 
+	// Posts
 	for (const post of posts) {
 		entries.push(
 			urlEntry(
 				absolute(`/posts/${post.slug}/`),
-				post.data.updated?.toISOString() ?? post.data.published.toISOString(),
+				post.data.updated?.toISOString() ??
+					post.data.published.toISOString(),
 				post.data.image
-					? { loc: absolute(post.data.image), title: post.data.title, caption: post.data.description || post.data.title }
-					: undefined,
+					? [
+							{
+								loc: absolute(post.data.image),
+								title: post.data.title,
+								caption:
+									post.data.description ??
+									post.data.title,
+							},
+						]
+					: [],
 			),
 		);
 	}
 
+	// Tags
 	for (const tag of tags) {
-		entries.push(urlEntry(absolute(`/tags/${encodeURIComponent(tag.name)}/`), now));
+		entries.push(
+			urlEntry(
+				absolute(`/tags/${encodeURIComponent(tag.name)}/`),
+				now,
+			),
+		);
 	}
 
+	// Categories
 	for (const category of categories) {
 		if (category.url.includes("/uncategorized/")) continue;
-		entries.push(urlEntry(absolute(`/categories/${encodeURIComponent(category.name)}/`), now));
+
+		entries.push(
+			urlEntry(
+				absolute(`/categories/${encodeURIComponent(category.name)}/`),
+				now,
+			),
+		);
 	}
 
-	entries.push(urlEntry(absolute(`/categories/uncategorized/`), now));
+	entries.push(
+		urlEntry(absolute("/categories/uncategorized/"), now),
+	);
 
+	// Achievement images
 	for (const achievement of achievements) {
 		if (achievement.data.photo) {
 			achievementImages.push({
-					loc: absolute(achievement.data.photo),
-					title: `${achievement.data.competition} ${achievement.data.award}`,
-					caption: achievement.data.year,
+				loc: absolute(achievement.data.photo),
+				title: `${achievement.data.competition} ${achievement.data.award}`,
+				caption: String(achievement.data.year),
 			});
 		}
 	}
 
-	entries.push(urlEntry(absolute("/achievements/"), now, achievementImages));
+	entries.push(
+		urlEntry(
+			absolute("/achievements/"),
+			now,
+			achievementImages,
+		),
+	);
 
 	const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+<urlset
+	xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+	xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
 ${entries.join("\n")}
 </urlset>`;
 
